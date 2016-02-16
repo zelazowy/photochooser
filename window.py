735f0aa86d58d2bc3f52e0573c6b33e3857f74a1
Jonczyk <krzysztof.jonczyk@goldenline.pl>
2016-02-16 21:13:01 +0100

    OOP

--- window.py
+++ window.py
@@ -2,17 +2,79 @@
 # -*- coding: utf-8 -*-
 #
 import sys
-from PyQt4.QtGui import *
+from PyQt4 import QtGui, QtCore
 import exifread
 import re
 import glob
 import os
 
+
+class Photochooser(QtGui.QWidget):
+    dir = ''
+    files = ''
+
+    def __init__(self):
+        super(Photochooser, self).__init__()
+
+        filename = self.getFirstFile()
+        self.showFile(filename)
+
+    def showFile(self, filename):
+        f = open(filename, 'rb')
+        tags = exifread.process_file(f)
+
+        try:
+            rotationTag = tags['Image Orientation']
+            print(rotationTag)
+
+            rotation = re.search('[0-9]+', str(rotationTag)).group(0)
+        except:
+            rotation = 0
+
+        label = QtGui.QLabel(self)
+        pixmap = QtGui.QPixmap(filename)
+        pixmap = pixmap.scaledToWidth(1200)
+        # pixmap = pixmap.transformed(QTransform.rotate(rotation))
+
+        transform = QtGui.QTransform()
+        transform.rotate(float(rotation))
+
+        pixmap = pixmap.transformed(transform)
+
+        label.setPixmap(pixmap)
+
+        self.resize(min(pixmap.width(), 1200), min(pixmap.height(), 800))
+
+        # buttons
+        # btn = QtGui.QPushButton('Hello World!', self)
+        # btn.setToolTip('Elo')
+        # btn.clicked.connect(exit)
+        # btn.resize(btn.sizeHint())
+        # btn.move(80, 100)
+
+        # Show window
+        self.show()
+
+    def getFirstFile(self):
+        while True:
+            self.files = glob.glob(os.path.join(self.openDir(), '*.jpg'))
+
+            try:
+                filename = self.files[0]
+                break
+            except IndexError:
+                pass
+
+        return filename
+
+    def openDir(self):
+        return QtGui.QFileDialog.getExistingDirectory()
+
 # Create an PyQT4 application object.
-a = QApplication(sys.argv)
+a = QtGui.QApplication(sys.argv)
 
 # The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QWidget()
+w = Photochooser()
 
 # Set window size.
 w.resize(320, 240)
@@ -20,57 +82,8 @@ w.resize(320, 240)
 # Set window title
 w.setWindowTitle("Hello World!")
 
-# Get filename using QFileDialog
-# filename = QFileDialog.getOpenFileName(w, 'Open File', '/')
-# print(filename)
 
-dir = QFileDialog.getExistingDirectory(w, 'Open directory')
-# print(dir)
 
-# gets all files in directory with given extension. Each filename contains directory name
-files = glob.glob(os.path.join(dir, '*.jpg'))
 
-# list all files in directory
-# for file in files:
-#     print(file)
-
-filename = files[0]
-print(filename)
-# sys.exit()
-
-f = open(filename, 'rb')
-tags = exifread.process_file(f)
-
-try:
-    rotationTag = tags['Image Orientation']
-    print(rotationTag)
-
-    rotation = re.search('[0-9]+', str(rotationTag)).group(0)
-except KeyError:
-    rotation = 0
-
-label = QLabel(w)
-pixmap = QPixmap(filename)
-pixmap = pixmap.scaledToWidth(1200)
-# pixmap = pixmap.transformed(QTransform.rotate(rotation))
-
-transform = QTransform()
-transform.rotate(float(rotation))
-
-pixmap = pixmap.transformed(transform)
-
-label.setPixmap(pixmap)
-
-w.resize(min(pixmap.width(), 1200), min(pixmap.height(), 800))
-
-# buttons
-btn = QPushButton('Hello World!', w)
-btn.setToolTip('Elo')
-btn.clicked.connect(exit)
-btn.resize(btn.sizeHint())
-btn.move(80, 100)
-
-# Show window
-w.show()
 
 sys.exit(a.exec_())
