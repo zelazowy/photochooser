0f6cfa7335678d89a2f2f9d736ad637aa24bb80d
Jonczyk <krzysztof.jonczyk@goldenline.pl>
2016-02-25 19:39:31 +0100

    names convention fix scaling images to maximized window RC

--- photochooser.py
+++ photochooser.py
@@ -8,204 +8,221 @@ import shutil
 
 
 class Photochooser(QtGui.QWidget):
-    WIDTH = 1200
-    HEIGHT = 800
+    app_width = 1200
+    app_height = 800
 
     dir = ''
-    copyDir = ''
-    deleteDir = ''
+    copy_dir = ''
+    delete_dir = ''
     files = []
-    currentFileIndex = 0
-    currentFile = ''
+    current_file_index = 0
+    current_file = ''
     label = None
 
     # buttons
-    copyBtn = None
-    deleteBtn = None
-    softDeleteBtn = None
+    copy_btn = None
+    delete_btn = None
+    soft_delete_btn = None
 
-    def __init__(self):
+    def __init__(self, screen_width, screen_height):
         super(Photochooser, self).__init__()
 
+        self.app_width = screen_width
+        self.app_height = screen_height
         self.label = QtGui.QLabel(self)
 
-        self.initButtons()
-        self.initFiles()
+        self.init_buttons()
+        self.init_files()
 
-        self.showFile(self.getFirstFile())
+        self.show_file(self.get_first_file())
 
-    def initFiles(self):
+        self.showMaximized()
+
+    def init_files(self):
         while True:
-            currentDir = self.openDir()
-            self.files = glob.glob(os.path.join(currentDir, '*.jpg'))
+            currentDir = self.open_dir()
+            self.files.extend(glob.glob(os.path.join(currentDir, '*.jpg')))
+            self.files.extend(glob.glob(os.path.join(currentDir, '*.png')))
 
             # if dir contains any images then break, continue choosing dir otherwise
             if 0 < len(self.files):
-                self.copyDir = os.path.join(currentDir, 'copy')
-                self.deleteDir = os.path.join(currentDir, 'delete')
-                print(self.copyDir)
+                self.copy_dir = os.path.join(currentDir, 'copy')
+                self.delete_dir = os.path.join(currentDir, 'delete')
+                print(self.copy_dir)
                 break
 
-    def initButtons(self):
+    def init_buttons(self):
         # buttons
-        self.copyBtn = QtGui.QPushButton('Copy!', self)
-        self.copyBtn.clicked.connect(lambda: self.copyPhoto())
-        self.copyBtn.resize(self.copyBtn.sizeHint())
-        self.copyBtn.move(5, 5)
+        self.copy_btn = QtGui.QPushButton('Copy!', self)
+        self.copy_btn.clicked.connect(lambda: self.copy_photo())
+        self.copy_btn.resize(self.copy_btn.sizeHint())
+        self.copy_btn.move(5, 5)
 
         # prevents button from getting focused, required for arrow support in windows
-        self.copyBtn.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.copy_btn.setFocusPolicy(QtCore.Qt.NoFocus)
 
-        self.deleteBtn = QtGui.QPushButton('Delete!', self)
-        self.deleteBtn.clicked.connect(lambda: self.deletePhoto())
-        self.deleteBtn.resize(self.deleteBtn.sizeHint())
-        self.deleteBtn.move(5, 30)
-
-        # prevents button from getting focused, required for arrow support in windows
-        self.deleteBtn.setFocusPolicy(QtCore.Qt.NoFocus)
-
-        self.softDeleteBtn = QtGui.QPushButton('Delete?', self)
-        self.softDeleteBtn.clicked.connect(lambda: self.softDeletePhoto())
-        self.softDeleteBtn.resize(self.softDeleteBtn.sizeHint())
-        self.softDeleteBtn.move(5, 55)
+        self.delete_btn = QtGui.QPushButton('Delete!', self)
+        self.delete_btn.clicked.connect(lambda: self.delete_photo())
+        self.delete_btn.resize(self.delete_btn.sizeHint())
+        self.delete_btn.move(5, 30)
 
         # prevents button from getting focused, required for arrow support in windows
-        self.softDeleteBtn.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.delete_btn.setFocusPolicy(QtCore.Qt.NoFocus)
 
-    def resetButtons(self):
-        self.copyBtn.setText('Copy!')
-        self.deleteBtn.setText('Delete!')
-        self.softDeleteBtn.setText('Delete?')
+        self.soft_delete_btn = QtGui.QPushButton('Delete?', self)
+        self.soft_delete_btn.clicked.connect(lambda: self.soft_delete_photo())
+        self.soft_delete_btn.resize(self.soft_delete_btn.sizeHint())
+        self.soft_delete_btn.move(5, 55)
 
-    def showFile(self, filename):
-        self.resetButtons()
+        # prevents button from getting focused, required for arrow support in windows
+        self.soft_delete_btn.setFocusPolicy(QtCore.Qt.NoFocus)
 
-        self.currentFile = filename
+    def reset_buttons(self):
+        self.copy_btn.setText('Copy!')
+        self.delete_btn.setText('Delete!')
+        self.soft_delete_btn.setText('Delete?')
+
+    def show_file(self, filename):
+        self.reset_buttons()
+
+        self.current_file = filename
 
         f = open(filename, 'rb')
 
         pixmap = QtGui.QPixmap(filename)
-        pixmap = pixmap.scaledToWidth(self.WIDTH)
 
         try:
             tags = exifread.process_file(f)
-            rotationTag = tags['Image Orientation']
-            # print(rotationTag)
+            rotation_tag = tags['Image Orientation']
 
-            rotation = re.search('[0-9]+', str(rotationTag)).group(0)
+            rotation = re.search('[0-9]+', str(rotation_tag)).group(0)
             transform = QtGui.QTransform()
             transform.rotate(float(rotation))
             pixmap = pixmap.transformed(transform)
         except:
             pass
 
+        # resize
+        factor = self.get_scale_factor(pixmap.width(), pixmap.height())
+        pixmap = pixmap.scaledToHeight(pixmap.height() * factor)
+
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setPixmap(pixmap)
-        self.resize(min(pixmap.width(), self.WIDTH), min(pixmap.height(), self.HEIGHT))
 
         # Show window
-        self.show()
+        # self.showMaximized()
 
-    def getFirstFile(self):
+    def get_scale_factor(self, img_width, img_height):
+        factor_w, factor_h = 1.0, 1.0
+        if img_width > self.app_width:
+            factor_w = self.app_width / img_width
+        if img_height > self.app_height:
+            factor_h = self.app_height / img_height
+
+        return min(factor_w, factor_h)
+
+    def get_first_file(self):
         return self.files[0]
 
-    def getNextFile(self):
+    def get_next_file(self):
         try:
-            nextFile = self.files[self.currentFileIndex + 1]
-            self.currentFileIndex += 1
+            next_file = self.files[self.current_file_index + 1]
+            self.current_file_index += 1
 
-            return nextFile
+            return next_file
         except:
             return False
 
-    def getPrevFile(self):
-        if 0 > self.currentFileIndex - 1:
+    def get_prev_file(self):
+        if 0 > self.current_file_index - 1:
             return False
 
         try:
-            prevFile = self.files[self.currentFileIndex - 1]
-            self.currentFileIndex -= 1
+            prev_file = self.files[self.current_file_index - 1]
+            self.current_file_index -= 1
 
-            return prevFile
+            return prev_file
         except:
             return False
 
-    def openDir(self):
+    def open_dir(self):
         return QtGui.QFileDialog.getExistingDirectory()
 
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Right or e.key() == QtCore.Qt.Key_Period:
-            nextFile = self.getNextFile()
-            # print(nextFile)
-            if False != nextFile:
-                self.showFile(nextFile)
+            next_file = self.get_next_file()
+            if False != next_file:
+                self.show_file(next_file)
         elif e.key() == QtCore.Qt.Key_Left or e.key() == QtCore.Qt.Key_Comma:
-            prevFile = self.getPrevFile()
-            # print(prevFile)
-            if False != prevFile:
-                self.showFile(prevFile)
+            prev_file = self.get_prev_file()
+            if False != prev_file:
+                self.show_file(prev_file)
         elif e.key() == QtCore.Qt.Key_C:
-            self.copyPhoto()
+            self.copy_photo()
         elif e.key() == QtCore.Qt.Key_D:
-            self.softDeletePhoto()
+            self.soft_delete_photo()
         elif e.key() == QtCore.Qt.Key_Backspace:
-            self.deletePhoto()
+            self.delete_photo()
 
         QtGui.QWidget.keyPressEvent(self, e)
 
-    def copyPhoto(self):
-        if not os.path.exists(self.copyDir):
-            os.makedirs(self.copyDir)
+    def copy_photo(self):
+        if not os.path.exists(self.copy_dir):
+            os.makedirs(self.copy_dir)
 
-        shutil.copy2(self.currentFile, self.copyDir)
+        shutil.copy2(self.current_file, self.copy_dir)
 
-        self.copyBtn.setText('Copied')
+        self.copy_btn.setText('Copied')
 
-    def softDeletePhoto(self):
-        if not os.path.exists(self.deleteDir):
-            os.makedirs(self.deleteDir)
+    def soft_delete_photo(self):
+        if not os.path.exists(self.delete_dir):
+            os.makedirs(self.delete_dir)
 
-        shutil.move(self.currentFile, self.deleteDir)
-        self.__afterDeletePhoto()
+        shutil.move(self.current_file, self.delete_dir)
+        self.__after_delete_photo()
 
-        self.softDeleteBtn.setText('Deleted')
+        self.soft_delete_btn.setText('Deleted')
 
-    def deletePhoto(self):
-        os.remove(self.currentFile)
-        self.__afterDeletePhoto()
+    def delete_photo(self):
+        os.remove(self.current_file)
+        self.__after_delete_photo()
 
-        self.deleteBtn.setText('Deleted')
+        self.delete_btn.setText('Deleted')
 
-    def __afterDeletePhoto(self):
-        del self.files[self.currentFileIndex]
+    def __after_delete_photo(self):
+        del self.files[self.current_file_index]
         # decrement current file index, because deleting file from list moves all next files one index down
         # now getting next file would get proper next file
-        self.currentFileIndex -= 1
+        self.current_file_index -= 1
 
-        nextFile = self.getNextFile()
-        if False != nextFile:
-            self.showFile(nextFile)
+        next_file = self.get_next_file()
+        if False != next_file:
+            self.show_file(next_file)
         else:
             # its a little tricky, but to get previous file the index must be incremented so getting prev file wold get proper previous file
-            self.currentFileIndex += 1
+            self.current_file_index += 1
 
-            prevFile = self.getPrevFile()
-            if False != prevFile:
-                self.showFile(prevFile)
+            prev_file = self.get_prev_file()
+            if False != prev_file:
+                self.show_file(prev_file)
             else:
-                t = QtGui.QMessageBox.information(
+                QtGui.QMessageBox.information(
                     self,
                     'No files in directory',
                     'Chosen directory does not contain any images',
                     QtGui.QMessageBox.Ok
                 )
-                print(t)
                 exit()
+
 
 # Create an PyQT4 application object.
 a = QtGui.QApplication(sys.argv)
 
+screen_resolution = a.desktop().screenGeometry()
+screen_width, screen_height = screen_resolution.width(), screen_resolution.height()
+
 # The QWidget widget is the base class of all user interface objects in PyQt4.
-w = Photochooser()
+w = Photochooser(screen_width - 20, screen_height - 50)
 
 # Set window title
 w.setWindowTitle("Photo Chooser")
